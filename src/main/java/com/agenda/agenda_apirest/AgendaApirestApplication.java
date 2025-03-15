a13f118367be28624df784f1897b932563834947
Maxi <maximorojas002@gmail.com>
2025-03-15 20:47:04 -0300

    setting program

---
+++ src/main/java/com/agenda/agenda_apirest/AgendaApirestApplication.java
@@ -0,0 +1,13 @@
+package com.agenda.agenda_apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaApirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agenda/agenda_apirest/AgendaApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agenda.agenda_apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
